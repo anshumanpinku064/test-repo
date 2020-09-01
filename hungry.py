8d1c11bfbe8c25aa3e5ba21d8e4b70a25d684cd1
anshuman <anshumanpinku064@gmail.com>
2020-09-01 13:32:38 +0200

    Second Version of Hungry Code with user input

--- hungry.py
+++ hungry.py
@@ -1,3 +1,6 @@
 #https://github.com/anshumanpinku064/test-repo.git
-
-print('I am hungry!')
+hungry = input('Are you hungry?')
+if hungry == 'yes':
+    print('Have Samosa! :)')
+else:
+    print('Come back when you are! :)')
